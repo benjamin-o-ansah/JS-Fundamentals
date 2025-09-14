3f3dd2c986d716e0291bac0a63b21f66e701a8fc
benjamin-o-ansah <benjaminoansah@gmail.com>
2025-09-14 23:11:49 +0000

    reverted code to the way it was at first

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,17 +1,8 @@
-const argv = process.argv.slice(2);
-// if (argv == 1) {
-//   console.log(argv[3]);
-// } else if (argv[2]) {
-//   console.log(argv[2]);
-// } else {
-//   console.log("No argument");
-// }
-
-
-const args = process.argv.slice(2);
-
-if (args[0] === undefined) {
-  console.log("No argument");
+const argv = process.argv;
+if (argv[2] && argv[3]) {
+  console.log(argv[2]);
+} else if (argv[2]) {
+  console.log(argv[2]);
 } else {
-  args.forEach(arg => console.log(arg));
+  console.log("No argument");
 }
