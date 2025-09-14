c52a71594e7dc8273626213f78b370d5ea196425
benjamin-o-ansah <benjaminoansah@gmail.com>
2025-09-14 23:06:34 +0000

    reworked on 3-arguments and pushed to github

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,8 +1,17 @@
-const argv = process.argv;
-if (argv[2] && argv[3]) {
-  console.log(argv[3]);
-} else if (argv[2]) {
-  console.log(argv[2]);
-} else {
+const argv = process.argv.slice(2);
+// if (argv == 1) {
+//   console.log(argv[3]);
+// } else if (argv[2]) {
+//   console.log(argv[2]);
+// } else {
+//   console.log("No argument");
+// }
+
+
+const args = process.argv.slice(2);
+
+if (args[0] === undefined) {
   console.log("No argument");
+} else {
+  args.forEach(arg => console.log(arg));
 }
